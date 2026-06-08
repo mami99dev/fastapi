60d0622930a040074c4c7f87594a5cc384d99880
mami-caprepol <isaacmar24@gmail.com>
2026-06-08 12:59:42 -0600

    conditionals

---
+++ 02-fundamentos-python/03-conditionals.py
@@ -0,0 +1,12 @@
+is_new_dev = False
+interested_in_python = True
+knows_python = False
+
+if is_new_dev:
+  print('Te recomiendo saber Python')
+elif interested_in_python:
+  print('Aprende Python en Devtalles')
+else:
+  print('Inicia en el mundo de la programacion')
+if knows_python:
+  print('Te recomiento aprender FastAPI')
