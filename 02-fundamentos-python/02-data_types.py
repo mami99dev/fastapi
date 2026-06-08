40c438c804d4de62e8677ef2234c6864b53070da
mami-caprepol <isaacmar24@gmail.com>
2026-06-08 12:51:10 -0600

    data types

---
+++ 02-fundamentos-python/02-data_types.py
@@ -0,0 +1,17 @@
+# Strings
+name = 'Isaac'
+full_name = 'Isaac Martinez'
+
+# Integers
+number = 200
+age = 26
+waste = 25
+
+# Floats
+decimal = 20.42
+temp = 27.3
+
+# Booleans
+boolean = True # 1
+boolean_false = False # 0
+is_gamer = False
