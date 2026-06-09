63f1acff238ec8e6bcce128c9f5dbfd10718979d
mami-caprepol <isaacmar24@gmail.com>
2026-06-09 12:01:54 -0600

    sets

---
+++ 02-fundamentos-python/08-sets.py
@@ -0,0 +1,9 @@
+# Un set es una coleccion desordenada de elementos unicos, es decir, sin indices y no permite duplicados
+my_set = {1, 2, 3, 4, 5, 6, 7, 8, 3, 4, 2, 1, 4, 8, 9, 1, 2, 6, 5, 10}
+# Python elimina los duplicados e imprime
+# {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# Usar sets cuando necesitamos que algo no se duplique
+usernames = {'Ricardo', 'Fernando', 'Isaac', 'Fernando', 'Isaac'}
+print(my_set)
+print(usernames)
