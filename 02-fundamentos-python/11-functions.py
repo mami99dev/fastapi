1167660d5a75f3ba33920e2bfe5d0d207a4719c6
mamicaprepol <mamidev99@gmail.com>
2026-08-17 10:47:11 -0600

    args and kwargs

--- 02-fundamentos-python/11-functions.py
+++ 02-fundamentos-python/11-functions.py
@@ -9,3 +9,24 @@ hello()
 
 # Keyword arguments
 hello(name='Juan', greet='Hi')
+
+# Args and Kwargs
+def add(*args, **kwargs):
+  print(args)
+  print(kwargs)
+  print(type(args))
+  print(type(kwargs))
+  result = 0
+  element = 0
+  while element < len(args):
+    result = result + args[element]
+    element = element + 1
+    
+  element = 0
+  
+  for element in kwargs:
+    result = result + kwargs[element]
+  return result
+  
+result = add(1, 2, 3, 4, 5, 6, 7, arg1=8, arg2=9, arg3=10)
+print(result)
